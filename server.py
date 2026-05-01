9d2f76e66169822d89c1a2cd58505640c9d0f89b
suppritha-collab <supprithaab@gmail.com>
2026-05-01 22:45:35 +0530

    Update server.py

--- server.py
+++ server.py
@@ -3,17 +3,18 @@ from EmotionDetection import emotion_detector
 
 app = Flask(__name__)
 
+
 @app.route("/emotionDetector")
 def detect_emotion():
     text = request.args.get('textToAnalyze')
 
-    # Error handling for blank input
     if text is None or text.strip() == "":
         return "Invalid input", 400
 
     result = emotion_detector(text)
 
     return jsonify(result)
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
